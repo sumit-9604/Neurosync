209dc892d74fb667ff03a4585b9636a05cf3444e
Piyush <Piyushmevliwal1241@gmail.com>
2026-06-08 21:05:52 +0530

    Fix type_text payload format and esc key name

--- NeuroSyncMobile/src/services/commandService.ts
+++ NeuroSyncMobile/src/services/commandService.ts
@@ -1,9 +1,10 @@
 import {api} from './apiClient';
 
 export const sendCommand = async (deviceId: string, action: string, args: object = {}) => {
-  const response = await api.post(`/api/v1/devices/${deviceId}/command`, {
+  const response = await api.post('/api/v1/command', {
+    device_id: deviceId,
     action,
-    ...args,
+    payload: args,
   });
   return response.data;
 };
